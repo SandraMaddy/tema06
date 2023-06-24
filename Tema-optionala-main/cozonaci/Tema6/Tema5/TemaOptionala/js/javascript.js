85f059e66a705fb38523d610e6b7c0a2dd699e93
SandraMaddy <sym_madys@yahoo.com>
2023-06-24 22:45:25 +0300

    Proiect final

--- Tema-optionala-main/cozonaci/Tema6/Tema5/TemaOptionala/js/javascript.js
+++
@@ -1,7 +0,0 @@
-const table = document.getElementById("myTable");
-const cells = table.querySelectorAll("td");
-
-for (let i = 0; i < cells.length; i++) {
-  const value = cells[i].textContent;
-  console.log(`Valoarea celulei ${i+1} este ${value}`);
-}
